18718387a4d4d8a739f07375f3dafd654afba560
Matthew Tytel <matthewtytel@gmail.com>
2024-10-14 09:13:29 -0400

    Embedding with cmake instead of cpp.

---
+++ visage_file_embed/create_embed_header.cmake
@@ -0,0 +1,25 @@
+if (NOT DEFINED FILE_LIST)
+  message(FATAL_ERROR "Required FILE_LIST is not set")
+endif ()
+
+if (NOT DEFINED DEST_FILE)
+  message(FATAL_ERROR "Required DEST_FILE is not set")
+endif ()
+
+if (NOT DEFINED VAR_NAMESPACE)
+  message(FATAL_ERROR "Required VAR_NAMESPACE is not set")
+endif ()
+
+file(WRITE ${DEST_FILE} "// Generated file, do not edit\n")
+file(APPEND ${DEST_FILE} "#pragma once\n\n")
+file(APPEND "${DEST_FILE}" "#include \"embedded_file.h\"\n")
+file(APPEND "${DEST_FILE}" "#include <string>\n")
+file(APPEND "${DEST_FILE}" "namespace ${VAR_NAMESPACE} {\n")
+file(APPEND "${DEST_FILE}" "  ::visage::EmbeddedFile getFileByName(const std::string& filename);\n")
+
+foreach (FILE ${FILE_LIST})
+  string(REGEX MATCH "([^/]+)$" VAR_NAME ${FILE})
+  string(REGEX REPLACE "\\.| |-" "_" VAR_NAME ${VAR_NAME})
+  file(APPEND "${DEST_FILE}" "  extern ::visage::EmbeddedFile ${VAR_NAME};\n")
+endforeach ()
+file(APPEND "${DEST_FILE}" "}\n")
---
+++ visage_file_embed/embed_file.cmake
@@ -0,0 +1,25 @@
+if (NOT DEFINED ORIGINAL_FILE)
+  message(FATAL_ERROR "Required ORIGINAL_FILE is not set")
+endif ()
+
+if (NOT DEFINED DEST_FILE)
+  message(FATAL_ERROR "Required DEST_FILE is not set")
+endif ()
+
+if (NOT DEFINED VAR_NAMESPACE)
+  message(FATAL_ERROR "Required VAR_NAMESPACE is not set")
+endif ()
+
+string(REGEX MATCH "([^/]+)$" VAR_NAME ${ORIGINAL_FILE})
+string(REGEX REPLACE "\\.| |-" "_" VAR_NAME ${VAR_NAME})
+
+file(READ ${ORIGINAL_FILE} DATA HEX)
+string(REGEX REPLACE "([0-9a-f][0-9a-f])" "0x\\1," DATA ${DATA})
+file(WRITE "${DEST_FILE}" "// Generated file, do not edit\n")
+file(APPEND "${DEST_FILE}" "#include \"embedded_file.h\"\n")
+file(APPEND "${DEST_FILE}" "namespace ${VAR_NAMESPACE} {\n")
+file(APPEND "${DEST_FILE}" "static const char ${VAR_NAME}_name[] = \"${VAR_NAME}\";\n")
+file(APPEND "${DEST_FILE}" "static const unsigned char ${VAR_NAME}_tmp[] = {${DATA}};\n")
+file(APPEND "${DEST_FILE}" "::visage::EmbeddedFile ${VAR_NAME} = { ${VAR_NAME}_name, ")
+file(APPEND "${DEST_FILE}" "(const char*)${VAR_NAME}_tmp, sizeof(${VAR_NAME}_tmp) };\n")
+file(APPEND "${DEST_FILE}" "}")
