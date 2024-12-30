f129ae58f047f049632b3a2bd0f3dbcc632ef4fd
Matt Tytel <matt@vital.audio>
2024-12-30 12:57:35 -0500

    More emscripten tweaking.

--- visage_windowing/emscripten/windowing_emscripten.cpp
+++ visage_windowing/emscripten/windowing_emscripten.cpp
@@ -218,18 +218,18 @@ namespace visage {
       delta_x *= kPreciseScrollingScale;
       delta_y *= kPreciseScrollingScale;
     }
-    // int x = event->mouse.targetX - EM_ASM_INT({
-    //       var canvas = document.getElementById('canvas');
-    //       var rect = canvas.getBoundingClientRect();
-    //       return rect.left;
-    //     });
-// 
-    // int y = event->mouse.targetY - EM_ASM_INT({
-    //           var canvas = document.getElementById('canvas');
-    //           var rect = canvas.getBoundingClientRect();
-    //           return rect.top;
-    //         });
-    window->handleMouseWheel(delta_x, delta_y, delta_x, delta_y, event->mouse.targetX, event->mouse.targetX,
+    int x = event->mouse.targetX - EM_ASM_INT({
+          var canvas = document.getElementById('canvas');
+          var rect = canvas.getBoundingClientRect();
+          return rect.left;
+        });
+
+    int y = event->mouse.targetY - EM_ASM_INT({
+              var canvas = document.getElementById('canvas');
+              var rect = canvas.getBoundingClientRect();
+              return rect.top;
+            });
+    window->handleMouseWheel(delta_x, delta_y, delta_x, delta_y, x, y,
                              mouseButtonState(&event->mouse), keyboardModifiers(&event->mouse));
     return true;
   }
