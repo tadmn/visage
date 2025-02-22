5edb71ad32f840b5f12a0473961167944c2308fd
Matt Tytel <matt@vital.audio>
2025-02-22 15:10:54 -0500

    example colors.

--- examples/Basic/basic.cpp
+++ examples/Basic/basic.cpp
@@ -29,13 +29,13 @@ int runExample() {
   visage::ApplicationWindow app;
 
   app.onDraw() = [&app](visage::Canvas& canvas) {
-    canvas.setColor(0xff006600);
+    canvas.setColor(0xff000066);
     canvas.fill(0, 0, app.width(), app.height());
 
     float circle_radius = app.height() * 0.1f;
     float x = app.width() * 0.5f - circle_radius;
     float y = app.height() * 0.5f - circle_radius;
-    canvas.setColor(0xff00ff00);
+    canvas.setColor(0xff00ffff);
     canvas.circle(x, y, 2.0f * circle_radius);
   };
 
